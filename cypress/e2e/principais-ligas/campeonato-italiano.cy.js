ab485c55f02cbeb9b94da2a0d87f5f274a508e41
João Teodozo <joaoteodozou@outlook.com>
2024-10-28 20:03:12 -0300

    ajuste

--- cypress/e2e/principais-ligas/campeonato-italiano.cy.js
+++ cypress/e2e/principais-ligas/campeonato-italiano.cy.js
@@ -6,7 +6,7 @@ dayjs.locale(locale); // Define a localidade
 describe('Cálculo de Probabilidade de Gols no Campeonato Italiano', () => {
     const telegramToken = '8024146594:AAF7_cVi3by_5eYbYjOji9VnC-TFCayFVgM'; // Substitua pelo seu token
     const chatIds = [
-        '6617740232',
+        //'6617740232',
         '-4519143208',
         //'6662950862',
     ];
@@ -22,7 +22,7 @@ describe('Cálculo de Probabilidade de Gols no Campeonato Italiano', () => {
             const jogosHoje = $dia.closest('.ScheduleTables').find('.Table__TR--sm');
 
             // Verificar se há jogos
-            const numJogos = jogosHoje.length; // Armazena o número de jogos
+            const numJogos = jogosHoje.length;
 
             if (numJogos === 0) {
                 cy.log(`Não há jogos programados para ${diaAtual}.`);
@@ -36,6 +36,20 @@ describe('Cálculo de Probabilidade de Gols no Campeonato Italiano', () => {
                 const time2 = $jogo.find('.Table__Team:not(.away) a').text().trim(); // Nome do segundo time
                 const jogoLink = $jogo.find('.local a').attr('href'); // Link para a página do jogo
 
+                // Verifica se o jogo está "LIVE" ou "FT"
+                const isLive = $jogo.find('.Schedule__live').length > 0;
+                const isFinalizado = $jogo.find('.teams__col a:contains("FT")').length > 0;
+
+                if (isLive) {
+                    cy.log(`O jogo ${time1} x ${time2} está ao vivo. Ignorando o cálculo.`);
+                    return; // Ignora este jogo se estiver "LIVE"
+                }
+
+                if (isFinalizado) {
+                    cy.log(`O jogo ${time1} x ${time2} já está finalizado. Ignorando o cálculo.`);
+                    return; // Ignora este jogo se já estiver finalizado
+                }
+
                 // Acesse a página do jogo
                 cy.visit(`https://www.espn.com.br${jogoLink}`, { timeout: 300000 }).then(() => {
                     let jogosTime1 = 0;
@@ -46,57 +60,46 @@ describe('Cálculo de Probabilidade de Gols no Campeonato Italiano', () => {
                     let time2GolsSofridos = 0;
 
                     cy.get('h2.ScoreCell__TeamName').then(($h2s) => {
-                        // Captura os registros dos times
                         const recordTime1 = $h2s.eq(0).closest('.Gamestrip__TeamContent').find('.Gamestrip__Record').text();
                         const recordTime2 = $h2s.eq(1).closest('.Gamestrip__TeamContent').find('.Gamestrip__Record').text();
 
-                        // Função para somar os números
                         const somaJogos = (record) => {
                             const numeros = record.split(',')[0].split('-').map(Number);
                             return numeros.reduce((acc, num) => acc + num, 0);
                         };
 
-                        // Calcula os jogos de cada time
                         jogosTime1 = somaJogos(recordTime1);
                         jogosTime2 = somaJogos(recordTime2);
 
                         cy.get('.LOSQp.Kiog.TSdsN.lEHQF.Pxeau.FOePw.nbAEp').each(($el) => {
-                            // Verifica se a div contém "Total de gols"
                             const totalGolsElem = $el.find('span:contains("Total de gols")');
                             if (totalGolsElem.length > 0) {
-                                // Captura os gols marcados
                                 const marcadosDiv = $el.find('.UGvDX.ubOdK.WtEci.FfVOu.seFhp');
                                 time1GolsMarcados += parseInt(marcadosDiv.eq(0).find('span').text());
                                 time2GolsMarcados += parseInt(marcadosDiv.eq(1).find('span').text());
                             }
 
-                            // Verifica se a div contém "Gols sofridos"
                             const golsSofridosElem = $el.find('span:contains("Gols sofridos")');
                             if (golsSofridosElem.length > 0) {
-                                // Captura os gols sofridos
                                 const sofridosDiv = $el.find('.UGvDX.ubOdK.WtEci.FfVOu.seFhp');
                                 time1GolsSofridos += parseInt(sofridosDiv.eq(0).find('span').text());
                                 time2GolsSofridos += parseInt(sofridosDiv.eq(1).find('span').text());
                             }
                         }).then(() => {
-                            // Calcular as médias
                             const mediaGolsMarcadosTime1 = time1GolsMarcados / jogosTime1;
                             const mediaGolsSofridosTime1 = time1GolsSofridos / jogosTime1;
                             const mediaGolsMarcadosTime2 = time2GolsMarcados / jogosTime2;
                             const mediaGolsSofridosTime2 = time2GolsSofridos / jogosTime2;
 
-                            // Calcular a média total de gols
                             const mediaTotal = (mediaGolsMarcadosTime1 + mediaGolsSofridosTime1 + mediaGolsMarcadosTime2 + mediaGolsSofridosTime2) / 2;
 
-                            // Calcular a probabilidade
                             let probabilidade;
                             if (mediaTotal >= 4) {
                                 probabilidade = 100;
                             } else {
-                                probabilidade = (mediaTotal / 4) * 100; // Proporcional
+                                probabilidade = (mediaTotal / 4) * 100;
                             }
 
-                            // Log das médias e da probabilidade
                             cy.log(`Média de Gols Marcados pelo ${time1}: ${mediaGolsMarcadosTime1.toFixed(2)}`);
                             cy.log(`Média de Gols Sofridos pelo ${time1}: ${mediaGolsSofridosTime1.toFixed(2)}`);
                             cy.log(`Média de Gols Marcados pelo ${time2}: ${mediaGolsMarcadosTime2.toFixed(2)}`);
@@ -104,9 +107,8 @@ describe('Cálculo de Probabilidade de Gols no Campeonato Italiano', () => {
                             cy.log(`Média Total: ${mediaTotal.toFixed(2)}`);
                             cy.log(`Probabilidade de sair 2 ou mais gols: ${probabilidade.toFixed(2)}%`);
 
-                            // Mensagem a ser enviada
                             const message = `
-<b>CAMPEONATO ITALIANO</b>                            
+<b>CAMPEONATO BRASILEIRO - SÉRIE A</b>                            
 <b>Jogo:</b> ${time1} x ${time2}
 
 <b>Média de Gols Marcados pelo <u>${time1}</u>:</b> ${mediaGolsMarcadosTime1.toFixed(2)}
@@ -118,14 +120,13 @@ describe('Cálculo de Probabilidade de Gols no Campeonato Italiano', () => {
 <b>Probabilidade de sair 2 ou mais gols:</b> <u>${probabilidade.toFixed(2)}%</u>
 `;
 
-                            // Verificar se a probabilidade é 60% ou mais
                             if (probabilidade >= 60) {
                                 chatIds.forEach(id => {
                                     cy.request({
                                         method: 'POST',
                                         url: `https://api.telegram.org/bot${telegramToken}/sendMessage`,
                                         body: {
-                                            chat_id: id, // Aqui você envia o chat_id atual do loop
+                                            chat_id: id,
                                             text: message,
                                             parse_mode: 'HTML'
                                         }
